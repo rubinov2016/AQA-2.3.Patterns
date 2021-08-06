eb9b6c0a829ac85e8ecf755fc88505e51b071bac
Ivan Rubinov <lrubinov@yandex.ru>
2021-08-06 19:31:20 +0300

    fifth launch

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -3,7 +3,6 @@ package ru.netology.data;
 import com.github.javafaker.Faker;
 import lombok.Value;
 import lombok.val;
-//import org.jetbrains.annotations.NotNull;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -14,14 +13,9 @@ import java.util.Random;
 
 public class DataGenerator {
 
-    //   private static Faker faker;
-    //    private String locale = "ru/netology,delivery";
-    private String locale = "ru/netology,delivery";
-
     private DataGenerator() {
     }
 
-    //   public static @NotNull
     public static String generateDate(int shift) {
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
@@ -29,17 +23,43 @@ public class DataGenerator {
         date = date.plusDays(shift);
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String output = date.format(dtf);
-//        String output = dtf.format(date);
-//
         return output;
     }
 
     public static String generateCity(String locale) {
         // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
         // с помощью Faker, либо используя массив валидных городов и класс Random
-        Faker faker = new Faker(new Locale(locale));
-        String city = faker.address().cityName();
-        return city;
+        String[] cityList = {
+                "Москва",
+                "Омск",
+                "Томск",
+                "Петропавловск-Камчатский",
+                "Сочи",
+                "Краснодар",
+                "Красноярск",
+                "Великий Новгород",
+                "Екатеринбург",
+                "Йошкар-Ола",
+                "Калининград",
+                "Кемерово",
+                "Киров",
+                "Кострома",
+                "Курган",
+                "Курск",
+                "Санкт-Петербург",
+                "Сыктывкар",
+                "Чебоксары",
+                "Великий Новгород",
+                "Владивосток",
+                "Владикавказ",
+                "Волгоград",
+                "Вологда",
+                "Севастополь",
+                "Ставрополь",
+                "Ярославль"
+        };
+        int rnd = new Random().nextInt(cityList.length)
+        return cityList[rnd];
     }
 
     public static String generateName(String locale) {
@@ -64,11 +84,7 @@ public class DataGenerator {
 
         public static UserInfo generateUser(String locale) {
             // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
             UserInfo user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
-//            user.city = generateCity(locale);
-//            user.name = generateName(locale);
-//            user.phone = generatePhone(locale);
             return user;
         }
     }
